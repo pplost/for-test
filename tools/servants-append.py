34082949cfecb0ab98d9ed42357838d9a70fffb8
pplost <pplost@qq.com>
2017-09-09 08:03:43 +0800

    test

--- tools/servants-append.py
+++ tools/servants-append.py
@@ -12,8 +12,8 @@ with open('servants.json','r',encoding='utf-8') as rpoint:
 with open('servants-append.json','r',encoding='utf-8') as rpoint:
 	jsa = json.load(rpoint)
 	
-sortList=["seq","id","name","class","atk","hp","busterCards","artsCards","quickCards","busterHits","artsHits","quickHits","exHits","npHits","busterNP","artsNP","quickNP","exNP","npNP","npCard","busterEff","artsEff","quickEff","npEff","hitedNP","dropStar","starEff","critDamage","attaDamage"]
-
+#sortList=["seq","id","name","class","atk","hp","busterCards","artsCards","quickCards","busterHits","artsHits","quickHits","exHits","npHits","busterNP","artsNP","quickNP","exNP","npNP","npCard","busterEff","artsEff","quickEff","npEff","hitedNP","dropStar","starEff","critDamage","attaDamage"]
+sortList=["seq","id","name","class","busterCards","artsCards","quickCards","busterHits","artsHits","quickHits","exHits","npHits","busterNP","artsNP","quickNP","exNP","npNP","npCard","busterEff","artsEff","quickEff","npEff","hitedNP"]
 l=[]
 
 for x in js:
@@ -30,7 +30,7 @@ for x in js:
 
 outStr=json.dumps(l,ensure_ascii=False,indent=4)
 outStr=outStr.replace('    ','\t')
-with open('servants.json','w+',encoding='utf-8') as wpoint:
+with open('servants_new.json','w+',encoding='utf-8') as wpoint:
 	wpoint.write(outStr)
 # with open('servants.json','w+',encoding='utf-8') as wpoint:
 	# json.dump(l,wpoint,ensure_ascii=False,indent=4)
