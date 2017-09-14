c2f9eb9cc55e27cb190b9225bf3f37f8108d3464
pplost <pplost@qq.com>
2017-09-14 13:47:22 +0800

    test

--- js/title_link.js
+++ js/title_link.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
 	var pages = {
 		' ' : '首页',
 		'fcraft.html' : '羁绊礼装表',
@@ -7,18 +7,17 @@ $(document).ready(function(){
 		'exp_cal.html' : '狗粮计算器'
 	};
 	var page = window.location.pathname + window.location.search;
-	if(page.split('/')[2]){
+	if (page.split('/')[2]) {
 		page = page.split('/')[2];
-		for (var i in pages){
-			if(page == i){
+		for (var i in pages) {
+			if (page == i) {
 				continue;
 			}
 			$("#title_link").append('<a href="' + './' + i + '">' + pages[i] + '</a>&nbsp;&nbsp;');
 		}
-	}
-	else{
-		for (var i in pages){
-			if(i == ' '){
+	} else {
+		for (var i in pages) {
+			if (i == ' ') {
 				continue;
 			}
 			$("#contents").append('<h3><a href="' + i + '">' + pages[i] + '</a></h3>');
