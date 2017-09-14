6692e78097dcab4f2b10595282970beed3784e85
pplost <pplost@qq.com>
2017-09-14 10:41:21 +0800

    test

--- js/title_link.js
+++ js/title_link.js
@@ -1,17 +1,17 @@
 $(document).ready(function(){
 	var pages = {
-		'/index' : '首页',
-		'/index/fcraft.html' : '羁绊礼装表',
-		'/index/fcraft.html?no_pic' : '羁绊礼装表(无图版)',
-		'/index/np_cal.html' : 'NP计算器',
-		'/index/exp_cal.html' : '狗粮计算器'
+		' ' : '首页',
+		'/fcraft.html' : '羁绊礼装表',
+		'/fcraft.html?no_pic' : '羁绊礼装表(无图版)',
+		'/np_cal.html' : 'NP计算器',
+		'/exp_cal.html' : '狗粮计算器'
 	};
 	var page = window.location.pathname + window.location.search;
-	
+	page = page.split('/')[2];
 	for(var i in pages){
 		if(page == i){
 			continue;
 		}
-		$(#"title_link").append('<a href="' + i + '">' + pages[i] + '</a>');
+		$("#title_link").append('<a href="' + '.' + i + '">' + pages[i] + '</a>&nbsp;&nbsp;');
 	}
 });
