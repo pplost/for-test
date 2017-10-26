537d158e1754ece3b9d2fb79b2993ddb2f6409de
pplost <pplost@qq.com>
2017-10-26 16:09:58 +0800

    test

--- js/common.js
+++ js/common.js
@@ -1,7 +1,22 @@
 function numLenFormat(num, length) {
-  var s = "";
-  for (var i = 0; i < length; i++) {
-    s += "0";
-  }
-  return (s + num).slice(-length);
+	var s = Array(length).join(0) + num;
+	return s.slice(-length);
+}
+
+function readLocalJson(timeProperty,dataProperty,url) {
+	var currTimeStamp = Date.parse(new Date()) / 1000;
+	if (window.localStorage && window.localStorage.hasOwnProperty(dataProperty)&& window.localStorage.hasOwnProperty(timeProperty)) {
+		var lastTimeStamp=window.localStorage.getItem(timeProperty);
+		//1星期内直接读本机缓存
+		if(currTimeStamp-lastTimeStamp<604800){
+			console.log("读取本机缓存");
+			return JSON.parse(window.localStorage.getItem(dataProperty));
+		}
+	}
+	$.getJSON(url, function(data) {
+		window.localStorage.setItem(timeProperty, currTimeStamp);
+		window.localStorage.setItem(dataProperty, JSON.stringify(data));
+		console.log("读取json");
+		return data;
+	});
 }
--- js/items.js
+++ js/items.js
@@ -1,127 +1,128 @@
-$(document).ready(function() {
-  if (window.location.search == "") {
-    createItemList();
-  } else {
-    createItemReqInf();
-  }
+$(document).ready(function () {
+	if (window.location.search == "") {
+		createItemList();
+	} else {
+		createItemReqInf();
+	}
 });
 
 function createItemList() {
-  var itemList = [];
-  for (var i in itemsDict) {
-    if (i >= 6000 && i <= 8000) {
-      itemList.push(i);
-    }
-  }
-  itemList.sort();
-  var u = $("<ul></ul>");
-  u.attr("class", "display");
-  for (var i in itemList) {
-    var liStr = '<li><a href="require.html?' + itemList[i] + '"><img src="resources/items/' + itemList[i] + '.png" /></a></li>';
-    u.append(liStr);
-  }
-  $("#main").append(u);
+	var itemList = [];
+	for (var i in itemsDict) {
+		if (i >= 6000 && i <= 8000) {
+			itemList.push(i);
+		}
+	}
+	itemList.sort();
+	var u = $("<ul></ul>");
+	u.attr("class", "display");
+	for (var i in itemList) {
+		var liStr = '<li><a href="require.html?' + itemList[i] + '"><img src="resources/items/' + itemList[i] + '.png" /></a></li>';
+		u.append(liStr);
+	}
+	$("#main").append(u);
 }
 
 function createItemReqInf() {
-  var limitNum = 0;
-  var skillNum = 0;
-  var totalList = [];
-  var itemId = window.location.search.match(/\d+/g);
-  if (!itemsDict[itemId]) {
-    window.location.href = "require.html";
-    return;
-  }
-  document.title = itemsDict[itemId];
-  var tb = $("<table></table>");
-  tb.attr("style", "margin:10px 10px");
-  tb.append("<tr><td rowspan='6' style='width:140px'><img src='resources/items/" + itemId + ".png'</></td><td>AP效率前5</td><td>掉率前5</td></tr>");
-  tb.append("<tr><td>鶸鷄祈祷中</td><td>鶸鷄祈祷中</td></tr>");
-  tb.append("<tr><td>鶸鷄祈祷中</td><td>鶸鷄祈祷中</td></tr>");
-  tb.append("<tr><td>鶸鷄祈祷中</td><td>鶸鷄祈祷中</td></tr>");
-  tb.append("<tr><td>鶸鷄祈祷中</td><td>鶸鷄祈祷中</td></tr>");
-  tb.append("<tr><td>鶸鷄祈祷中</td><td>鶸鷄祈祷中</td></tr>");
-  $("#main").append(tb);
-  $.getJSON("https://pplost.github.io/for-test/data/data.json", function(data) {
-    $.each(data, function(i, info) {
-      if (info["id"] > 0) {
-        var singleLmtNum = 0;
-        var singleSklNum = 0;
-        var single = [];
-        $.each(info["limitItems"], function(j, items) {
-          $.each(items, function(k, item) {
-            if (item[0] == itemId) {
-              limitNum += item[2];
-              singleLmtNum += item[2];
-            }
-          });
-        });
-        $.each(info["SkillItems"], function(j, items) {
-          $.each(items, function(k, item) {
-            if (item[0] == itemId) {
-              skillNum += (item[2] * 3);
-              singleSklNum += item[2];
-            }
-          });
-        });
-        if (singleLmtNum > 0 || singleSklNum > 0) {
-          single.push(info["id"], singleLmtNum, singleSklNum * 3);
-          totalList.push(single);
-        }
-      }
-    });
-    createItemReqList(totalList, limitNum, skillNum);
-  });
+	var limitNum = 0;
+	var skillNum = 0;
+	var totalList = [];
+	var itemId = window.location.search.match(/\d+/g);
+	if (!itemsDict[itemId]) {
+		window.location.href = "require.html";
+		return;
+	}
+	document.title = itemsDict[itemId];
+	var tb = $("<table></table>");
+	tb.attr("style", "margin:10px 10px");
+	tb.append("<tr><td rowspan='6' style='width:140px'><img src='resources/items/" + itemId + ".png'</></td><td>AP效率前5</td><td>掉率前5</td></tr>");
+	tb.append("<tr><td>鶸鷄祈祷中</td><td>鶸鷄祈祷中</td></tr>");
+	tb.append("<tr><td>鶸鷄祈祷中</td><td>鶸鷄祈祷中</td></tr>");
+	tb.append("<tr><td>鶸鷄祈祷中</td><td>鶸鷄祈祷中</td></tr>");
+	tb.append("<tr><td>鶸鷄祈祷中</td><td>鶸鷄祈祷中</td></tr>");
+	tb.append("<tr><td>鶸鷄祈祷中</td><td>鶸鷄祈祷中</td></tr>");
+	$("#main").append(tb);
+	var data = readLocalJson("fgo_archive_data_time", "fgo_archive_data", "https://pplost.github.io/for-test/data/data.json");
+	console.log(data);
+	$.each(data, function (i, info) {
+		if (info["id"] > 0) {
+			var singleLmtNum = 0;
+			var singleSklNum = 0;
+			var single = [];
+			$.each(info["limitItems"], function (j, items) {
+				$.each(items, function (k, item) {
+					if (item[0] == itemId) {
+						limitNum += item[2];
+						singleLmtNum += item[2];
+					}
+				});
+			});
+			$.each(info["SkillItems"], function (j, items) {
+				$.each(items, function (k, item) {
+					if (item[0] == itemId) {
+						skillNum += (item[2] * 3);
+						singleSklNum += item[2];
+					}
+				});
+			});
+			if (singleLmtNum > 0 || singleSklNum > 0) {
+				single.push(info["id"], singleLmtNum, singleSklNum * 3);
+				totalList.push(single);
+			}
+		}
+	});
+	createItemReqList(totalList, limitNum, skillNum);
+
 }
 
 function createItemReqList(totalList, limitNum, skillNum) {
-  var t = $("<table></table>");
-  t.attr("class", "no_side_border");
-  if (limitNum > 0) {
-    var tr = $("<tr></tr>");
-    tr.append("<td style='width:100px;padding-top:25px'>突破总共需要素材：" + limitNum + "</td>");
-    var td = $("<td></td>");
-    var ul = $("<ul></ul>");
-    for (var i in totalList) {
-      if (totalList[i][1] > 0) {
-        var li = $("<li></li>");
-        var pic_div = $("<div></div>");
-        var num_div = $("<div></div>");
-        pic_div.attr("class", "bg_pic");
-        num_div.attr("class", "float_num");
-        pic_div.append("<a href='http://fgowiki.com/guide/petdetail/" + totalList[i][0] + "'><img src='http://file.fgowiki.fgowiki.com/fgo/head/" + numLenFormat(totalList[i][0], 3) + ".jpg'></a>");
-        num_div.append("<a href='http://fgowiki.com/guide/petdetail/" + totalList[i][0] + "'>" + totalList[i][1] + "</a>");
-        pic_div.append(num_div);
-        li.append(pic_div);
-        ul.append(li);
-      }
-    }
-    td.append(ul);
-    tr.append(td);
-    t.append(tr);
-  }
-  if (skillNum > 0) {
-    var tr = $("<tr></tr>");
-    tr.append("<td style='width:100px;padding-top:25px'>技能总共需要素材：" + skillNum + "</td>");
-    var td = $("<td></td>");
-    var ul = $("<ul></ul>");
-    for (var i in totalList) {
-      if (totalList[i][2] > 0) {
-        var li = $("<li></li>");
-        var pic_div = $("<div></div>");
-        var num_div = $("<div></div>");
-        pic_div.attr("class", "bg_pic");
-        num_div.attr("class", "float_num");
-        pic_div.append("<a href='http://fgowiki.com/guide/petdetail/" + numLenFormat(totalList[i][0], 3) + "'><img src='http://file.fgowiki.fgowiki.com/fgo/head/" + numLenFormat(totalList[i][0], 3) + ".jpg'></a>");
-        num_div.append(totalList[i][2]);
-        pic_div.append(num_div);
-        li.append(pic_div);
-        ul.append(li);
-      }
-    }
-    td.append(ul);
-    tr.append(td);
-    t.append(tr);
-  }
-  $("#main").append(t);
+	var t = $("<table></table>");
+	t.attr("class", "no_side_border");
+	if (limitNum > 0) {
+		var tr = $("<tr></tr>");
+		tr.append("<td style='width:100px;padding-top:25px'>突破总共需要素材：" + limitNum + "</td>");
+		var td = $("<td></td>");
+		var ul = $("<ul></ul>");
+		for (var i in totalList) {
+			if (totalList[i][1] > 0) {
+				var li = $("<li></li>");
+				var pic_div = $("<div></div>");
+				var num_div = $("<div></div>");
+				pic_div.attr("class", "bg_pic");
+				num_div.attr("class", "float_num");
+				pic_div.append("<a href='http://fgowiki.com/guide/petdetail/" + totalList[i][0] + "'><img src='http://file.fgowiki.fgowiki.com/fgo/head/" + numLenFormat(totalList[i][0], 3) + ".jpg'></a>");
+				num_div.append("<a href='http://fgowiki.com/guide/petdetail/" + totalList[i][0] + "'>" + totalList[i][1] + "</a>");
+				pic_div.append(num_div);
+				li.append(pic_div);
+				ul.append(li);
+			}
+		}
+		td.append(ul);
+		tr.append(td);
+		t.append(tr);
+	}
+	if (skillNum > 0) {
+		var tr = $("<tr></tr>");
+		tr.append("<td style='width:100px;padding-top:25px'>技能总共需要素材：" + skillNum + "</td>");
+		var td = $("<td></td>");
+		var ul = $("<ul></ul>");
+		for (var i in totalList) {
+			if (totalList[i][2] > 0) {
+				var li = $("<li></li>");
+				var pic_div = $("<div></div>");
+				var num_div = $("<div></div>");
+				pic_div.attr("class", "bg_pic");
+				num_div.attr("class", "float_num");
+				pic_div.append("<a href='http://fgowiki.com/guide/petdetail/" + numLenFormat(totalList[i][0], 3) + "'><img src='http://file.fgowiki.fgowiki.com/fgo/head/" + numLenFormat(totalList[i][0], 3) + ".jpg'></a>");
+				num_div.append(totalList[i][2]);
+				pic_div.append(num_div);
+				li.append(pic_div);
+				ul.append(li);
+			}
+		}
+		td.append(ul);
+		tr.append(td);
+		t.append(tr);
+	}
+	$("#main").append(t);
 }
