4abf91458de32789765bf05a64c2e8da52692f8f
pplost <pplost@qq.com>
2017-10-26 23:14:43 +0800

    test

--- js/common.js
+++ js/common.js
@@ -1,19 +1,19 @@
 function numLenFormat(num, length) {
-  var s = Array(length).join(0) + num;
-  return s.slice(-length);
+    var s = Array(length).join(0) + num;
+    return s.slice(-length);
 }
 
 function readJson(url) {
-  var readData;
-  $.ajax({
-    url: url,
-    type: "get",
-    async: false,
-    dataType: "json",
-    cache: true,
-    success: function(data) {
-      readData=data;
-    },
-  });
-  return readData;
+    var readData;
+    $.ajax({
+        url: url,
+        type: "get",
+        async: false,
+        dataType: "json",
+        cache: true,
+        success: function(data) {
+            readData = data;
+        },
+    });
+    return readData;
 }
