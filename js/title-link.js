a0f941c1ead59900932ed05bc3665e8221df1113
pplost <pplost@qq.com>
2017-11-03 11:18:13 +0800

    test

--- js/title-link.js
+++ js/title-link.js
@@ -9,17 +9,29 @@ $(document).ready(function() {
         'drop_chance.html': '效率剧场'
     };
     var page = window.location.pathname + window.location.search;
-    page = page.split('/')[2];
-    var e_ul = $("<ul></ul>");
-    e_ul.attr("class", "nav navbar-nav");
-    for (var i in pages) {
-        var e_li = $("<li></li>");
-        if (page == i) {
-            $("#page_name").append(pages[i]);
-            e_li.attr("class", "active");
+    page = page.split('/')[2].replace(/\?\d+/g, '');
+    //主页目录
+    if (page == "" || page == "index.html") {
+        for (var i in pages) {
+            if (i == "index.html") {
+                continue;
+            }
+            $("#contents").append("<h3><a href='" + i + "'>" + pages[i] + "</a></h3>");
         }
-        e_li.append("<a href='" + i + "'>" + pages[i] + "</a>");
-        e_ul.append(e_li);
     }
-    $("#navbar").append(e_ul);
+    //子页导航
+    else {
+        var e_ul = $("<ul></ul>");
+        e_ul.attr("class", "nav navbar-nav");
+        for (var i in pages) {
+            var e_li = $("<li></li>");
+            if (page == i) {
+                $("#page_name").append(pages[i]);
+                e_li.attr("class", "active");
+            }
+            e_li.append("<a href='" + i + "'>" + pages[i] + "</a>");
+            e_ul.append(e_li);
+        }
+        $("#navbar").append(e_ul);
+    }
 });
